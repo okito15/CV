5a70456bc83cd2f59b6093ecff8a17d6f25d0d26
Okan Seid <necrorex11@gmail.com>
2022-05-15 04:24:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Okan Seid's CV
+
+https://okito15.github.io/CV
+
+Huge thanks to startbootstrap.com for the free CV template. Check it out here -> https://startbootstrap.com/theme/resume
---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', event => {
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+});
